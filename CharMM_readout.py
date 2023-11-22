99cd6cdbcc26dc98705c432818d40fd3dacd7d64
n3sty <siemerinkjob@gmail.com>
2023-11-22 10:36:26 +0100

    Aangepast voor compatibiliteit met de Pi

--- CharMM_readout.py
+++ CharMM_readout.py
@@ -14,9 +14,9 @@ import threading #For reading out the sensors every .5 seconds.
 
 #Connecting to the different instruments. Both COM port and node have to be specified.
 #Additional sensors can be added here.
-bl100 = propar.instrument('COM4', 3)
-diffp = propar.instrument('COM8', 4)
-coriflow = propar.instrument('COM8', 5)
+bl100 = propar.instrument("/dev/ttyUSB2", 7)
+diffp = propar.instrument("/dev/ttyUSB2", 4)
+coriflow = propar.instrument("/dev/ttyUSB2", 5)
 
 
 #Defining the readout function, which reads out the sensors.
@@ -37,14 +37,21 @@ def readout():
 
     #This function has multiple outputs: the sensors' measurements and the time of measurement.
     readoutdata = (thetime,T_BL100, MF_BL100, Rho_BL100, T_Cori, MF_Cori, Rho_Cori, DP)
+    
+    print(readoutdata)
+    
+    coriflow.wink(3)
+    diffp.wink(3)
+    bl100.wink(3)
+
     return readoutdata
 
 
 #Initializing dataframe and values.
 df = pd.DataFrame(columns=['Time','T_BL100', 'MF_BL100', 'Rho_BL100', 'T_Cori', 'MF_Cori', 'Rho_Cori', 'DP'])
-iteration = 0 #Loop iteration starts at index 0.
-windowsize = 21 #This determines the scale of the graphs, adding 1 increases the window by .5 seconds.
-path="C:\\Data\\" #This is the path where the csv file and graphs will be written to.
+iteration = 0 					#Loop iteration starts at index 0.
+windowsize = 21 				#This determines the scale of the graphs, adding 1 increases the window by .5 seconds.
+path="/home/flow-setup/Desktop/Data/" 		#This is the path where the csv file and graphs will be written to.
 
 
 #Defining the startup function, which adds readout data to the dataframe without plotting.
@@ -78,50 +85,50 @@ def mainprint():
     #...which is then printed to a csv file.
     df.to_csv(path+"output.csv", index=False)
 
-    #Writing the plots in jpeg format.
-    #The script takes the last X rows from the database, corresponding to the window size of the plot.
-    gdf = df.tail(windowsize)
+    		#Writing the plots in jpeg format.
+    		#The script takes the last X rows from the database, corresponding to the window size of the plot.
+    # gdf = df.tail(windowsize)
 
-    #Temerature plot
-    tempplot = gdf.plot(x='Time',y=['T_Cori', 'T_BL100'],grid=True, xticks = [0,5,10,15,20])
-    tempplot.set_ylabel('Temperature (degC)')
-    tempplot.set_xlabel('Time')
-    tempplot.legend(['Cori','BL100'],loc='upper left')
-    plt.savefig(path+"Temp.jpeg") #This is the file name, located in the specified path.
+    		#Temerature plot
+    # tempplot = gdf.plot(x='Time',y=['T_Cori', 'T_BL100'],grid=True, xticks = [0,5,10,15,20])
+    # tempplot.set_ylabel('Temperature (degC)')
+    # tempplot.set_xlabel('Time')
+    # tempplot.legend(['Cori','BL100'],loc='upper left')
+    # plt.savefig(path+"Temp.jpeg") #This is the file name, located in the specified path.
 
-    #To prevent potential errors, the plot is closed again.
-    plt.close('all')
+    		#To prevent potential errors, the plot is closed again.
+    # plt.close('all')
 
-    #Mass flow plot
-    mfplot = gdf.plot(x='Time',y=['MF_Cori', 'MF_BL100'],grid=True, xticks = [0,5,10,15,20])
-    mfplot.set_ylabel('Mass Flow (g/h)')
-    mfplot.set_xlabel('Time')
-    mfplot.legend(['Cori','BL100'],loc='upper left')
-    plt.savefig(path+"MF.jpeg")
+    		#Mass flow plot
+    # mfplot = gdf.plot(x='Time',y=['MF_Cori', 'MF_BL100'],grid=True, xticks = [0,5,10,15,20])
+    # mfplot.set_ylabel('Mass Flow (g/h)')
+    # mfplot.set_xlabel('Time')
+    # mfplot.legend(['Cori','BL100'],loc='upper left')
+    # plt.savefig(path+"MF.jpeg")
 
-    plt.close('all')
+    # plt.close('all')
 
-    #Density plot
-    rhoplot = gdf.plot(x='Time',y=['Rho_Cori', 'Rho_BL100'],grid=True, xticks = [0,5,10,15,20])
-    rhoplot.set_ylabel('Density (kg/m^3)')
-    rhoplot.set_xlabel('Time')
-    rhoplot.legend(['Cori','BL100'],loc='upper left')
-    plt.savefig(path+"Rho.jpeg")
+    		#Density plot
+    # rhoplot = gdf.plot(x='Time',y=['Rho_Cori', 'Rho_BL100'],grid=True, xticks = [0,5,10,15,20])
+    # rhoplot.set_ylabel('Density (kg/m^3)')
+    # rhoplot.set_xlabel('Time')
+    # rhoplot.legend(['Cori','BL100'],loc='upper left')
+    # plt.savefig(path+"Rho.jpeg")
 
-    plt.close('all')
+    # plt.close('all')
 
-    #DP sensor plot
-    dpplot = gdf.plot(x='Time',y='DP',grid=True, xticks = [0,5,10,15,20])
-    dpplot.set_ylabel('Pressure (mbar)')
-    dpplot.set_xlabel('Time')
-    plt.savefig(path+"DP.jpeg")
+    		#DP sensor plot
+    # dpplot = gdf.plot(x='Time',y='DP',grid=True, xticks = [0,5,10,15,20])
+    # dpplot.set_ylabel('Pressure (mbar)')
+    # dpplot.set_xlabel('Time')
+    # plt.savefig(path+"DP.jpeg")
 
-    plt.close('all')
+    # plt.close('all')
 
-    iteration = iteration + 1
+    # iteration = iteration + 1
 
 #Running the mainprint function every .5 seconds.
 while True:
-    time.sleep(0.5) #It runs the mainprint function every .5 seconds.
+    time.sleep(0.5) 		#It runs the mainprint function every .5 seconds.
     t = threading.Thread(target=mainprint,args=())
     t.start()
