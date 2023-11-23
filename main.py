8f7d0be222343ef602bffc79129107c3789c7ee1
n3sty <107898501+n3sty@users.noreply.github.com>
2023-11-23 12:54:45 +0100

    Commented all code

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-# Importing requiered packages
-import pandas as pd # Data is stored in a Pandas dataframe
+# Importing required packages
+import pandas as pd                         # Data is stored in a Pandas dataframe
 import numpy as np 
 import datetime
 import time
-import propar # The Bronkhorst sensor reading package
+import propar                               # The Bronkhorst sensor reading package
 import threading
 
 
@@ -51,16 +51,24 @@ class Sensor:
 
 
 def main():
+    """
+    Main function encapsulates all other functions that provide the functionality
+    for the script. The main first initializes all variables and structures, and
+    then runs the while loop for all updates and data gathering functions.
+    """
+    
     global bl100, diffp, coriflow, iteration, df, path
    
     # Connecting the instruments. Both USB port (tty**** on Linux, COM* on Windows) 
     # and node have to be specified. Additional sensors can also be added here.
-    bl100 = Sensor("/dev/ttyUSB2", 7) 
-    diffp = Sensor("/dev/ttyUSB2", 4)
-    coriflow = Sensor("/dev/ttyUSB2", 5)    
+    bl100 = Sensor("/dev/ttyUSB2", 7)       # bl100 Sensor location and node
+    diffp = Sensor("/dev/ttyUSB2", 4)       # Pressure drop Sensor location and node
+    coriflow = Sensor("/dev/ttyUSB2", 5)    # Coriolis flow Sensor location and node
     
+    # Dataframe of pandas has a nice structure which requires no further changes for the output file.
     df = pd.DataFrame(columns=["Time", "T_BL100", "MF_BL100", "RHO_BL100", "T_CORI", "MF_CORI", "RHO_CORI", "DP"])
     
+    # Certain constants that influence the data gathering and the length/size of the measurement.
     dataFrequency = 4                       # Number of data samples per second
     iteration = 0                           # Loop iteration starts at index 0
     gatherTime = 10                         # Time (sec) of data gathering
@@ -76,10 +84,12 @@ def main():
         
 
 
-# The readout function reads out the sensors and exports to a .csv file.
+# The readout function reads out the sensors on specified parameters and exports to a .csv file.
 def Readout():
-    """ Reads the data from the preformatted sensors
-        # TODO: incorporate compatibility with all types of sensors
+    """ 
+    Reads the data from the preformatted sensors
+    Returns a tuple of defined data variables
+    # TODO: incorporate compatibility with all types of sensors
     """
 
     # Getting the time of the measurement
@@ -90,13 +100,18 @@ def Readout():
     [T_CORI, MF_CORI, RHO_CORI] = coriflow.readMultiple([142, 205, 270])
     DP = diffp.readSingle(205)
     
+    # Concatenating results into a single data variable
     data = (t, T_BL100, MF_BL100, RHO_BL100, T_CORI, MF_CORI, RHO_CORI, DP)
     
     return data
 
 
 def UpdateDataframe():
-    
+    """
+    Function designed to be simple and quick, to run every data-gather-period.
+    TODO: Check if the current method of changing the dataframe works efficiently.
+    Returns nothing.    
+    """
     df.loc[iteration] = list(Readout())
     
     iteration += 1
@@ -105,6 +120,12 @@ def UpdateDataframe():
 
 
 def WriteData():
+    """
+    Function designed to be simple and quick, to run every data-gather-period.
+    Writes the data gathered in the last iteration to a .csv file.
+    TODO: Don't write every period, only write when the gathering is done.
+    Returns nothing.
+    """
     
     df.to_csv(path+"output.csv", index=False)
     
