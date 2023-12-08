2aa7b3428e6019e41e23555658f22a98850cdfb9
vortexwave <luukkm@gmail.com>
2023-12-08 13:26:45 +0100

    sensor controler to get stuff out of main

---
+++ testmain.py
@@ -0,0 +1,54 @@
+import pandas as pd                         # Data is stored in a Pandas dataframe
+import datetime                             
+import time
+from Sensor import Sensor
+from arduino_readout import PressTemp
+import sensor_controler
+import warnings
+import threading
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+global dataFrequency, gatherTime
+
+# TODO: all defined functinos except main in different files
+
+def main():
+    """
+    Main function encapsulates all other functions that proSvide the functionality
+    for the script. The main first initializes all variables and structures, and
+    then runs the while loop for all updates and data gathering functions.
+    """
+    global iteration, gatherTime, dataFrequency, path
+
+    # Certain constants that influence the data gathering and the length/size of the measurement.
+    dataFrequency = 10                                           # Number of data samples per second
+    iteration = 0                                                # Loop iteration starts at index 0
+    gatherTime = 60                                              # Time (sec) of data gathering
+    path = "/home/flow-setup/Desktop/UltraSonicFluids/Data"      # Output location on the raspberry pi
+       
+    # Runs the initialize function to read out all the sensors
+    sensor_controler.initialize()
+    #sensor_controler.initialize()
+    
+    # Loop containing al the update functions for reading data.
+    # TODO: Remove sleep, to keep the time in between data gathers usable.
+    iterations = gatherTime * dataFrequency
+
+    while iteration < iterations:
+        try:
+            threadUpdate = threading.Thread(target=sensor_controler.updateDataframe, args=())
+            threadUpdate.start()
+            
+            time.sleep(1 / dataFrequency)       # Runs every 1/f period        
+        except KeyboardInterrupt:
+            sensor_controler.writeData(path=path)
+            break
+    
+    # Write data to the defined path into a csv file
+    sensor_controler.writeData(path=path)
+    
+    return 0        
+
+
+if __name__ == "__main__":
+    main()
