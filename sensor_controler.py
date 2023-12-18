497d2e73656ea9502356a984353ed0ca29a2ce7e
vortexwave <luukkm@gmail.com>
2023-12-18 12:30:39 +0100

    date monrth day

--- sensor_controler.py
+++ sensor_controler.py
@@ -34,7 +34,7 @@ def readout():
     # TODO: incorporate compatibility with all types of sensors (mflf ) coriolus werkt 
     """
     # Getting the time of the measurement
-    t = datetime.datetime.now().strftime("%H:%M:%S,%f")[:-5]
+    t = datetime.datetime.now().strftime("%m:%d:%H:%M,%f")[:-5]
 
     # Read out the desired parameters of each sensor
 
