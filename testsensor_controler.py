956cdbffb68087510d5392042dc05bcad4a3a9a8
vortexwave <luukkm@gmail.com>
2023-12-19 11:00:26 +0100

    baud

--- testsensor_controler.py
+++ testsensor_controler.py
@@ -22,7 +22,7 @@ def initialize():
     arduino = PressTemp()                                    # Arduino serial connection
     arduino.setup()                     # Initialises all Arduino sensors
 
-    pump_syringe_serial.PumpSyringe("/dev/ttyUSB4", 38400, x = 0, mode = 0, verbose=False)
+    pump_syringe_serial.PumpSyringe("/dev/ttyUSB4", 9600, x = 0, mode = 0, verbose=False)
 
     # Dataframe of pandas has a nice structure which requires no further changes for the output file.
     # TODO: Make dataframe and parameter collection automatically sizeable.
