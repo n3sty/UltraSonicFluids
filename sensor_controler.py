f7eaa49174fbf318c7f1a57249013811be23984c
tedvanwijk <tedvanwijk@gmail.com>
2023-12-19 11:33:36 +0100

    pls werk

--- sensor_controler.py
+++ sensor_controler.py
@@ -27,7 +27,7 @@ def initialize():
 #    df = pd.DataFrame(columns=["Time", "MF_LF", "T_CORI", "MF_CORI", "RHO_CORI", "P_DP", "Pin_DP", "Pout_DP", "Ard_P1", "Ard_T1", "Ard_P2", "Ard_T2", "Ard_P3", "Ard_T3"])
     df = pd.DataFrame(columns=['time', 'MF_LF', 'T_CORI', 'MF_CORI', 'RHO_CORI', 'P_DP'])
 
-    animationplot.initialize(df, 'MF_LF', 50)
+    animationplot.initialize()
     
     return 0
 
