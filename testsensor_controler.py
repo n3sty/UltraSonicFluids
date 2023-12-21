b6782889d8815f5b6bd8374540160c8159f18a86
vortexwave <luukkm@gmail.com>
2023-12-21 15:00:10 +0100

    s

--- testsensor_controler.py
+++ testsensor_controler.py
@@ -27,7 +27,6 @@ def initialize():
     syringe = pump_syringe_serial.PumpSyringe("/dev/ttyUSB3", 9600, x = 0, mode = 0, verbose=True)
 
     syringe.openConnection()
-    syringe.getResponse()
     syringe.startPump()
     time.sleep(2)
     syringe.stopPump()
