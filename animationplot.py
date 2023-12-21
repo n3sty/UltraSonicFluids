4e011e582ee0cfa1ce937d5e706d0ab394d4ce14
tedvanwijk <tedvanwijk@gmail.com>
2023-12-21 14:37:00 +0100

    oeps

--- animationplot.py
+++ animationplot.py
@@ -47,7 +47,7 @@ def initialize():
         plotTitle = 'Live DP'
         plotYlabel = 'DP [mbar]'
     # fig = plt.figure()
-    fig, ax = fig.add_subplots()
+    fig, ax = plt.add_subplots()
     line = ax.plot([], [])
     ani = FuncAnimation(fig, animate, interval=500, blit=False, cache_frame_data=False)
     plt.show()
