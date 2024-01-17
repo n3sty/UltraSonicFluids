519bf05a9595778535007192c1eab00af81e5edd
vortexwave <luukkm@gmail.com>
2024-01-17 09:39:07 +0100

    fix print

--- run_write.py
+++ run_write.py
@@ -67,7 +67,6 @@ def run_write(path, enable_arduino, enable_syringe):
             except KeyboardInterrupt:
                 date = datetime.datetime.now().strftime("%m-%d_%H%M")
                 df.to_csv(path + "/EXP_" + date + ".csv", index=False)
-                print('lalalalalal')
-                print(f'{path} + "/EXP_" + {date} + ".csv')
+                print(f'Saving the dataframe to: {path} + "/EXP_" + {date} + ".csv')
                 rewrite_syringe.stop(enable_syringe)
                 break #double break?
